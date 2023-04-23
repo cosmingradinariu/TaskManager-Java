46395baa84aa7b93f0d46c2fa4c01db8c17628b1
cosmingradinariu <126178291+cosmingradinariu@users.noreply.github.com>
2023-04-23 12:23:35 +0300

    modif

--- New folder/TaskManager.java
+++ New folder/TaskManager.java
@@ -6,23 +6,25 @@ public class TaskManager {
     
     void addTask(Task task) {
         tasks.add(task);
-        System.out.println("You added "+ task);
+        System.out.println("You added: "+ task);
         System.out.println(tasks);
     }
 
     void removeTask(Task task) {
         tasks.remove(task);
-        System.out.println("You removed " + task);
+        System.out.println("You removed: " + task);
         System.out.println(tasks);
     }
 
     void modifyTask(Task task, String name, String descritpion, String date, boolean finished) {
+        System.out.println("You modified: " + task);
         task.setName(name);
         task.setDescription(descritpion);
         task.setDate(date);
         task.setFinished(finished);
-        System.out.println("You modified " + task);
+        System.out.println("To: " + task);
         System.out.println(tasks);
+        
     }
 
     public String toString() {
--- New folder/project.java
+++ New folder/project.java
@@ -8,16 +8,13 @@ public class project {
     public static void main(String[] args) {
 
         //TaskGUI taskManager = new TaskGUI();
-        Task task1 = new Task("Washing", "Dishwashing", "25-08-1234", false);
-        System.out.println(task1);
-        
         TaskManager taskManager = new TaskManager();
-
-        taskManager.addTask(task1);
-        Task task2 = new Task("Nigger", "Nigger", "Nigger", true);
+        Task task1 = new Task("asa", "asd", "123", false);
+        Task task2 = new Task("asdasd", "asdasdas", "asdasda", true);
         taskManager.addTask(task2);
-        taskManager.removeTask(task2);
-        System.out.println(taskManager);
+        taskManager.addTask(task1);
+        taskManager.getTasks();
+        taskManager.modifyTask(task2, "dada", "dada", "dada", false);
 
     }
 }
